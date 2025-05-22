23d3966bad5ca8a0524955ec6448ec4dda2991bd
aprar-jalal <s12324682@stu.najah.edu>
2025-05-22 04:28:11 -0700

    just pushing

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,7 @@
 <?php
 
 use App\Models\UserApplicationJob;
-use App\Models\UserFavoriteJob;
+use App\Models\UserFavoriteJobs;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -9,7 +9,8 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::post('user/{user_id}/job',[UserFavoriteJob::class,'store']);
-Route::delete('user/{user_id}/job',[UserFavoriteJob::class,'destroy']);
+Route::post('user/{user_id}/job',[UserFavoriteJobs::class,'store']);
+Route::delete('user/{user_id}/job',[UserFavoriteJobs
+::class,'destroy']);
 
 Route::post('user/{user_id}/job',[UserApplicationJob::class,'store']);
