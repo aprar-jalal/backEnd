9d0a97f41ff17abe76aceb009ec10c934285d529
jana0025 <janaalqasim41@gmail.com>
2025-05-25 13:45:23 +0300

    migrations and models

--- database/migrations/2025_05_13_181244_create_users_table.php
+++ database/migrations/2025_05_13_181244_create_users_table.php
@@ -12,9 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-
-
-            $table->Integer('role_id');
+            $table->Id('role_id');
             $table->string('email')->unique();
             $table->string('password');
             $table->enum('gender', ['male', 'female']);
