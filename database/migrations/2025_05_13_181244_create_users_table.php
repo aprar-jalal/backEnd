3a712407aec22db6103f0f45c2a9fd99b90505da
jana0025 <janaalqasim41@gmail.com>
2025-05-25 13:59:54 +0300

    conflict

--- database/migrations/2025_05_13_181244_create_users_table.php
+++ database/migrations/2025_05_13_181244_create_users_table.php
@@ -12,7 +12,9 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id('role_id');
+            $table->id('user_id');
+
+
             $table->string('email')->unique();
             $table->string('password');
             $table->enum('gender', ['male', 'female']);
