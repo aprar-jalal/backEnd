6b487c71132bb593db2743b76e897e17c7d8d37c
jana0025 <janaalqasim41@gmail.com>
2025-05-26 14:37:40 +0300

    conflict

--- app/Models/User.php
+++ app/Models/User.php
@@ -29,12 +29,12 @@ class User extends Model{
 
 
     public function favoriteJob() : BelongsToMany{
-        return $this->belongsToMany(Job::class, 'user_favorite_jobs' ,'job_id ','role_id')
+        return $this->belongsToMany(Job::class, 'user_favorite_jobs' ,'job_id ','user_id')
             ->using(UserFavoriteJobs::class);
     }
 
     public Function AppliedJobs() : BelongsToMany{
-        return $this->belongsToMany(Job::class, 'user_application_job' ,'job_id ','role_id')
+        return $this->belongsToMany(Job::class, 'user_application_job' ,'job_id ','user_id')
             ->using(UserApplicationJob::class)
             ->withPivot('applicationStatus')
             ->withTimestamps();
