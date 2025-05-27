82c510bad979d84fd8d2334011413348fb106284
aprar-jalal <s12324682@stu.najah.edu>
2025-05-27 15:37:08 -0700

    new

--- routes/api.php
+++ routes/api.php
@@ -1,20 +1,27 @@
 <?php
 
 
+use App\Http\Controllers\JobController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\UserFavoriteJobsController;
 use App\Models\UserApplicationJob;
-use App\Models\UserFavoriteJob;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
-
-Route::post('user/{user_id}/job',[UserFavoriteJob::class,'store']);
-Route::delete('user/{user_id}/job',[UserFavoriteJob::class,'destroy']);
+//aprar
+Route::post('user/{user_id}/job',[UserFavoriteJobsController::class,'store']);
+Route::delete('user/{user_id}/job',[UserFavoriteJobsController::class,'destroy']);
 
 Route::post('user/{user_id}/job',[UserApplicationJob::class,'store']);
 
+Route::get('/allJobs',[JobController::class,'getAllJobs']);
+
+Route::get('/search', [JobController::class, 'search']);
+
+//end
 Route::post('/login', [UserController::class, 'manualLogin']);
 Route::get('/check-auth', [UserController::class, 'checkout']);
 Route::get('/current-user', [UserController::class, 'currentUser']);
